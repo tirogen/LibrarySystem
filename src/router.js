77280af6df98c61c72d5091acfed1e04bccbd522
NithiP <the-px@hotmail.com>
2019-11-15 00:41:01 +0700

    before add boostrap

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import VueDemo from '@/components/VueDemo'
 import Messages from '@/components/Messages'
+import Librarian from '@/components/Librarian'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/messages',
       name: 'messages',
       component: Messages
+    },
+    {
+      path: '/librarian',
+      name: 'librarian',
+      component: Librarian
     }
   ]
 })
