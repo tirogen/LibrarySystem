1a40cf14bcb1b4a26643cda7215615947aefd40c
B2B <yotsatorn@B2B.ST-244F>
2019-11-15 01:46:16 +0700

    update sth

--- node_modules/@vue/cli-service/generator/template/src/main.js
+++ node_modules/@vue/cli-service/generator/template/src/main.js
@@ -4,9 +4,5 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 new Vue({
-  <%_ if (doesCompile) { _%>
-  render: h => h(App),
-  <%_ } else { _%>
-  render: function (h) { return h(App) },
-  <%_ } _%>
+  render: h => h(App)
 }).$mount('#app')
