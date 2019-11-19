3396ce2634e716a71e8a9f923faf385315c3f3c8
Tanawit <tanawit139@gmail.com>
2019-11-19 10:54:32 +0700

    add messy navbar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <template>
-  <div id="app">
-    <b-container>
-      <h1>Library System</h1>
-      <div id="nav">
-       <router-link :to="{ name: 'home' }">Home</router-link> |
-       <router-link :to="{ name: 'student' }">Student</router-link> |
-       <router-link :to="{ name: 'librarian' }">Librarian</router-link>
-      </div>
+  <div id="app" class="mt-0">
+<!--    <b-container>-->
+<!--      <h1>Library System</h1>-->
+<!--      <div id="nav">-->
+<!--       <router-link :to="{ name: 'home' }">Home</router-link> |-->
+<!--       <router-link :to="{ name: 'student' }">Student</router-link> |-->
+<!--       <router-link :to="{ name: 'librarian' }">Librarian</router-link>-->
+<!--      </div>-->
+<!--      <router-view/>-->
+<!--    </b-container>-->
+    <Layout>
       <router-view/>
-    </b-container>
+    </Layout>
   </div>
 </template>
-
+<script>
+  import Layout from "./layout/Layout";
+  export default {
+      components: {Layout},
+      comments: {
+          Layout
+      }
+  }
+</script>
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
---
+++ src/layout/Layout.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <Navbar/>
+    <main id="main-container">
+      <div class="container">
+        <slot/>
+      </div>
+    </main>
+<!--    <footer class="LayoutDefault__footer">-->
+<!--      &copy; Awesome Company-->
+<!--    </footer>-->
+  </div>
+</template>
+
+<script>
+  import Navbar from "./Navbar";
+    export default {
+        name: "Layout",
+        components: {Navbar},
+        comments: {
+            Navbar,
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
