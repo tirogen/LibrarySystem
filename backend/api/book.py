559a848bce72a8c0e13e2503e2d00193d6a4aa3a
Thanawat Jierawatanakanok <taan02991@hotmail.com>
2019-11-23 16:16:52 +0700

    Return get

--- backend/api/book.py
+++ backend/api/book.py
@@ -3,25 +3,28 @@ from rest_framework.response import Response
 from django.db import connection
 from rest_framework import status
 
+def getBook():
+	statement = ("SELECT * FROM `api_isbn`AS i\
+						INNER JOIN `api_book` AS b ON b.Isbn_id=i.Isbn") 
+	cursor = connection.cursor()
+	cursor.execute(statement)
+	response = {}
+	for row in cursor.fetchall():
+		if row[0] in response.keys():
+			response[row[0]]["number"].append(row[4])
+		else:
+			response[row[0]] = {
+				"category": row[1],
+				"author": row[2],
+				"name": row[3],
+				"number": [row[4]]
+			}
+	return Response(response, status = status.HTTP_200_OK)
+
 @api_view(['GET', 'POST', 'DELETE'])
 def book(request, id=None):
 	if request.method == 'GET':
-		statement = ("SELECT * FROM `api_isbn`AS i\
-						INNER JOIN `api_book` AS b ON b.Isbn_id=i.Isbn") 
-		cursor = connection.cursor()
-		cursor.execute(statement)
-		response = {}
-		for row in cursor.fetchall():
-			if row[0] in response.keys():
-				response[row[0]]["number"].append(row[4])
-			else:
-				response[row[0]] = {
-					"category": row[1],
-					"author": row[2],
-					"name": row[3],
-					"number": [row[4]]
-				}
-		return Response(response, status = status.HTTP_200_OK)
+		return getBook()
 
 	elif request.method == 'DELETE':
 		cursor = connection.cursor()
@@ -44,13 +47,11 @@ def book(request, id=None):
 							VALUES ('Available', %s);")
 			cursor = connection.cursor()
 			cursor.execute(statement, [request.data["isbn"]])
-			insert_id = cursor.lastrowid
-			return Response({'id': insert_id, 'isbn': request.data["isbn"]}, status = status.HTTP_200_OK)
+			return getBook()
 		else:
 			statement = ("INSERT INTO `api_isbn`(`Isbn`, `Category`, `Author`, `Name`) VALUES (%s, %s, %s, %s);")
 			cursor = connection.cursor()
 			cursor.execute(statement, [request.data["isbn"], request.data["category"], request.data["author"], request.data["name"]])
 			statement = ("INSERT INTO `api_book`(`Status`,`Isbn_id`) VALUES ('Available', %s);")
 			cursor.execute(statement, [request.data["isbn"]])
-			insert_id = cursor.lastrowid
-			return Response({'id': insert_id, 'isbn': request.data["isbn"]}, status = status.HTTP_200_OK)
+			return getBook()
