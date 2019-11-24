ac06f0dbf1e766c18e0b2baf7143c3a1737e40f1
bsk <bee.bee09_@hotmail.co.th>
2019-11-24 18:22:59 +0700

    fix invalid

--- backend/api/borrowBook.py
+++ backend/api/borrowBook.py
@@ -15,37 +15,37 @@ def getBorrow():
                     INNER JOIN api_book ON api_borrow.Book_id=api_book.id\
                     INNER JOIN api_isbn ON api_book.Isbn_id=api_isbn.Isbn\
                     INNER JOIN api_booktime ON api_borrow.BookTime_id=api_booktime.id\
-					INNER JOIN api_student ON api_student.Username=api_borrow.Student_id\")
+					INNER JOIN api_student ON api_student.Username=api_borrow.Student_id")
     cursor = connection.cursor()
     cursor.execute(statement)
     response = []
     for row in cursor.fetchall():
-		obj = {
-			"studentID" : row[0],
-			"studentName" : row[1],
-			"studnetSurname" : row[2],
-			"startDate" : row[8],
-			"endDate" : row[9],
-			"bookISBN" : row[3],
-			"bookID": row[4],
-			"bookName" : row[5],
-			"bookCategory" : row[6],
-			"bookAuthor" : row[7],
-		}
-		# if row[0] in response.keys():
-        #     response[row[0]]["number"].append(row[4])
-        #     response[row[0]]["num"] = len(response[row[0]]["number"])
-        #     response[row[0]]["status"].append(row[5])
-        # else:
-        #     response[row[0]] = {
-        #         "enddate": row[1],
-        #         "author": row[2],
-        #         "name": row[3],
-        #         "number": [row[4]],
-        #         "status": [row[5]],
-        #         "num": 1
-        #     }
-		response.append(obj)
+        obj = {
+			"studentID": row[0],
+"studentName": row[1],
+"studnetSurname": row[2],
+"startDate": row[8],
+"endDate": row[9],
+"bookISBN": row[3],
+"bookID": row[4],
+"bookName": row[5],
+"bookCategory": row[6],
+"bookAuthor": row[7],
+        }
+        # if row[0] in response.keys():
+#     response[row[0]]["number"].append(row[4])
+#     response[row[0]]["num"] = len(response[row[0]]["number"])
+#     response[row[0]]["status"].append(row[5])
+# else:
+#     response[row[0]] = {
+#         "enddate": row[1],
+#         "author": row[2],
+#         "name": row[3],
+#         "number": [row[4]],
+#         "status": [row[5]],
+#         "num": 1
+#     }
+        response.append(obj)
     return Response(response, status=status.HTTP_200_OK)
 
 
@@ -63,5 +63,5 @@ def manageBorrowBook(request, id=None):
         cursor.execute(statement, [id])
         statement = ("SELECT * FROM `api_booktime` WHERE id=%s")
         cursor.execute(statement, [bookTimeID])
-        row = cursor.fetchall()
+        # row = cursor.fetchall()
         # iflen(row) == 0):
--- src/services/borrowService.js
+++ src/services/borrowService.js
@@ -2,13 +2,13 @@ import api from '@/services/api'
 
 export default {
   fetchbooks() {
-    return api.get('/book/book/')
+    return api.get('/borrow/borrow/')
       .then(response => {
         // console.log(response)
         return response.data})
   },
   deleteBook(id) {
-    return api.delete(`book/book/${id}/`, { timeout: 10000 })
+    return api.delete(`borrow/borrow/${id}/`, { timeout: 10000 })
       .then(response => response.data)
   },
 }
