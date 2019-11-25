1843b1eaccd31b6863dec7efb5cbe75c9905b4b3
Tanawit <tanawit139@gmail.com>
2019-11-25 20:42:02 +0700

    fix indent error

--- backend/api/student.py
+++ backend/api/student.py
@@ -5,18 +5,19 @@ from rest_framework import status
 
 
 def getStudentList():
-	statement = ("SELECT api_borrow.Student_id\
+    statement = ("SELECT api_borrow.Student_id\
                     FROM api_borrow\
 			        INNER JOIN api_student ON api_student.Username=api_borrow.Student_id")
-	cursor = connection.cursor()
-	cursor.execute(statement)
-	response = []
-	for row in cursor.fetchall():
+    cursor = connection.cursor()
+    cursor.execute(statement)
+    response = []
+    for row in cursor.fetchall():
         obj = {
-            "studentID":row[0],
-		}
-        response.append(obj)
-	return Response(response, status = status.HTTP_200_OK)
+            "studentID": row[0],
+        }
+    response.append(obj)
+    return Response(response, status=status.HTTP_200_OK)
+
 
 @api_view(['GET', 'POST', 'DELETE'])
 def studentList(request, id=None):
