ed4cc6fcfc22ca2d160b0ea2f0596d9ba84e3417
Tanawit <tanawit139@gmail.com>
2019-11-25 20:53:45 +0700

    add student name api

--- backend/api/student.py
+++ backend/api/student.py
@@ -1,6 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from django.db import connection
+from django.db import connection, Error
 from rest_framework import status
 
 
@@ -15,7 +15,7 @@ def getStudentList():
         obj = {
             "studentID": row[0],
         }
-    response.append(obj)
+        response.append(obj)
     return Response(response, status=status.HTTP_200_OK)
 
 
@@ -23,3 +23,24 @@ def getStudentList():
 def studentList(request, id=None):
     if request.method == 'GET':
         return getStudentList()
+
+
+@api_view(['GET'])
+def getStudentNameById(request, studentId):
+    statement = "SELECT FName, LName FROM api_student WHERE Username='{}'".format(studentId)
+    cursor = connection.cursor()
+    response = []
+    cursor.execute(statement)
+    try:
+        records = cursor.fetchall()
+        if len(records) == 0:
+            return Response([], status.HTTP_204_NO_CONTENT)
+        else:
+            for record in records:
+                response.append({
+                    'FirstName': record[0],
+                    'LastName' : record[1]
+                })
+            return Response(response, status=status.HTTP_200_OK)
+    except Error:
+        return Response([], status=status.HTTP_500_INTERNAL_SERVER_ERROR)
